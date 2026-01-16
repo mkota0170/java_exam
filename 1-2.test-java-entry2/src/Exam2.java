68e0e8de4f46b2e7517e950271c788953e6bcb25
kotaro <kotaro.matsuhana@rakus-partners.co.jp>
2026-01-16 11:10:23 +0900

    プログラミング入門2

---
+++ 1-2.test-java-entry2/src/Exam2.java
@@ -0,0 +1,9 @@
+public class Exam2 {
+    public static void main(String[] args) {
+        int roop = 1;
+        while(roop < 11){
+            System.out.println(roop +".松花虎太郎");
+            roop = roop + 1;
+        }
+    }
+}
