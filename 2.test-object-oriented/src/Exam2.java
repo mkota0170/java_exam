6b882387a0ccf745c3547aa772ec03d4b67047c7
kotaro <kotaro.matsuhana@rakus-partners.co.jp>
2026-01-20 09:48:17 +0900

    オブジェクト指向入門確認テスト

---
+++ 2.test-object-oriented/src/Exam2.java
@@ -0,0 +1,10 @@
+public class Exam2 {
+    public static void main(String[] args) {
+        
+        String[] day = {"月","火","水","木","金","土","日"};
+
+        for(String display : day){
+            System.out.println(display);
+        }
+    }
+}
---
+++ 2.test-object-oriented/src/Exam3.java
@@ -0,0 +1,10 @@
+public class Exam3 {
+    public static void main(String[] args) {
+        Student stu = new Student(50);
+
+        stu.study();
+        stu.study();
+        stu.sleep();
+        stu.sleep();
+    }
+}
